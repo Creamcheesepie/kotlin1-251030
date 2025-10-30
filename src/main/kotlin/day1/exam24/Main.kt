9aa39e5d682f6caee31f37eb80865405951894d0
Creamcheesepie <gnldbs1004@naver.com>
2025-10-30 14:40:13 +0900

    코틀린 스코프 함수 run

--- src/main/kotlin/day1/exam24/Main.kt
+++ src/main/kotlin/day1/exam24/Main.kt
@@ -1,6 +1,8 @@
 package day1.exam24
 
 fun main() {
+    //  원본 객체를 유지하며 원하는 작업을 할 때 사용
+    // DB 저장, 데이터 저장, 로그 출력 등의 작업에 사용된다.
     val length = "Hello".also{
         println("Before: $it")
     }.uppercase().also{
---
+++ src/main/kotlin/day1/exam25/Main.kt
@@ -0,0 +1,11 @@
+package day1.exam25
+
+fun main() {
+    val num = 5;
+
+    val rst = num.run{
+        this * 2 + 10;
+    }
+
+    println(rst)
+}
