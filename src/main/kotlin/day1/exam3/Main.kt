ba8eb1726e7efa81cf05df8989e12b6f6eab181c
Creamcheesepie <gnldbs1004@naver.com>
2025-10-30 09:47:15 +0900

    코틀린에는 삼항연산자가 없는대신 if문을 표현식으로 사용가능하다.

---
+++ src/main/kotlin/day1/exam3/Main.kt
@@ -0,0 +1,13 @@
+package day1.exam3
+
+fun main() {
+    val number = 10;
+    val str = if(number % 2 == 0) "even" else "odd";
+
+    println(str);
+
+    when{
+        number % 2 == 0 -> println("even");
+        number % 2 != 0 -> println("odd");
+    }
+}
