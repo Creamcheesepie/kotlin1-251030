49668f610658c28e9c247fbd2539e1259eccd325
Creamcheesepie <gnldbs1004@naver.com>
2025-10-30 11:38:16 +0900

    코틀린에서는 매개변수에 기본값을 적용하여 메소드 오버로딩과 유사한 효과를 낼 수 있다.

---
+++ src/main/kotlin/day1/exam13/Main.kt
@@ -0,0 +1,14 @@
+package day1.exam13
+
+
+fun sayHello(name : String = "guest" , age: Int = 20){
+    println("Hello $name!, Age $age");
+}
+
+fun main() {
+    sayHello()
+    sayHello("John");
+    sayHello("Denise",99);
+    sayHello(age = 20) ; // 매개변수 순서가 존재해서 앞의 매개변수를 비우고 뒤에 값을 넣을 때에는 매개변수 지정을 해줘야 한다.
+
+}
