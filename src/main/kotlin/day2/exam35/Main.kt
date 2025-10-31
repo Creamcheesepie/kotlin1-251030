413af6249b9453e826cb3b26c9cef5a4dff5e85c
Creamcheesepie <gnldbs1004@naver.com>
2025-10-31 09:51:04 +0900

    코드 동작 확인

--- src/main/kotlin/day2/exam35/Main.kt
+++ src/main/kotlin/day2/exam35/Main.kt
@@ -4,9 +4,17 @@ package day2.exam35
 class Person(){
     private var _name : String = ""
 
+    // 가상 필드
     var name : String
         get() = throw IllegalAccessException("읽을 수 없습니다.");
     set(value) {
         _name = value;
     }
+}
+
+fun main() {
+    val p = Person();
+    p.name = "Doe"
+
+    println(p.name);
 }
